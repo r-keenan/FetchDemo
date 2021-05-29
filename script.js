396c86d8cdf5528ca6155ef20f978bbf1661b781
Ross Keenan <ross.keenan@protonmail.com>
2021-05-29 17:39:41 -0500

    Completed fetch request for pulling data.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//Getting data from a server does not require the options in the fetch request.
 fetch("https://reqres.in/api/Users")
-  .then((res) => res.json())
+  .then((res) => {
+    if (res.ok) {
+      console.log("Success");
+    } else {
+      console.log("Error");
+    }
+    res.json();
+  })
   .then((data) => console.log(data));
+
+//
